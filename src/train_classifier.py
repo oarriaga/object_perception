b42397f97b9fac903b398d51119d8827dfb37654
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-03 17:33:33 +0200

    Changed training dataset parameters for VOC2007

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -8,11 +8,12 @@ from utils.utils import split_data
 from XML_parser import XMLParser
 
 # parameters
-batch_size = 7
+batch_size = 5
 num_epochs = 10000
 image_shape=(48, 48, 3)
 validation_split = .2
-dataset_root_path = '../datasets/german_open_dataset/'
+dataset_name = 'VOC2007'
+dataset_root_path = '../datasets/' + dataset_name + '/'
 annotations_path =  dataset_root_path + 'annotations/'
 image_prefix = dataset_root_path + 'images/'
 trained_models_path = '../trained_models/classification/simple_CNN'
@@ -23,7 +24,9 @@ data_manager = XMLParser(annotations_path)
 class_names = data_manager.class_names
 num_classes = len(class_names)
 print('Found classes: \n', class_names)
-ground_truth_data = data_manager.get_data()
+#ground_truth_data = data_manager.get_data()
+ground_truth_data = data_manager.get_data(['background', 'bottle'])
+print('Number of real samples:', len(ground_truth_data))
 
 # creating prior boxes
 prior_box_creator = PriorBoxCreator()
@@ -34,7 +37,8 @@ train_keys, val_keys = split_data(ground_truth_data, validation_split)
 image_generator = ImageGenerator(ground_truth_data, prior_box_manager,
                                 batch_size, image_shape[0:2], train_keys,
                                 val_keys, image_prefix,
-                                vertical_flip_probability=0)
+                                vertical_flip_probability=0,
+                                suffix='')
 
 # model parameters
 model = simple_CNN(image_shape, num_classes)
@@ -56,9 +60,6 @@ model_checkpoint = ModelCheckpoint(model_names,
 model.fit_generator(image_generator.flow(mode='train'),
                     steps_per_epoch=int(len(train_keys) / batch_size),
                     epochs=num_epochs, verbose=1,
-                    callbacks=[csv_logger, model_checkpoint],
+                    #callbacks=[csv_logger, model_checkpoint],
                     validation_data= image_generator.flow('val'),
                     validation_steps=int(len(val_keys) / batch_size))
-
-
-
